c2bc96b2fa7b2a92622b11775a963036b637563c
Mohamed Azzouz <azzouzminfo@yahoo.fr>
2017-02-08 20:22:14 +0100

    Resolve a bug I created...

--- jeu.c
+++ jeu.c
@@ -320,7 +320,7 @@ void key_pressed(KeySym code, char c, int x_souris, int y_souris)
 int main(int argc, char **argv) {
 
 	int width, height;
-	courFenetre = 2;
+	courFenetre = 0;
 	tour = 1;
 	joueur1 = JOUEUR;
 	joueur2 = IA;
